d4acf592e1fa5fe205807e0598b35719de28240e
Pitan <58260965+PTOM76@users.noreply.github.com>
2024-12-25 16:14:47 +0900

    Update index.php

--- index.php
+++ index.php
@@ -12,6 +12,9 @@ function getTotalStars($username) {
 
     $repos = json_decode($response, true);
 
+    if (isset($repos['status']) && $repos['status'] == "404" )
+      return 404;
+  
     $stars = 0;
     foreach ($repos as $repo) {
         $stars += $repo['stargazers_count'];
@@ -29,17 +32,28 @@ function base64_image($image) {
     return 'data:image/png;base64,' . base64_encode($encoded);
 }
 
+if (!isset($_GET['user'])) {
+  echo "Usage: https://github-pitan76-star-counter.glitch.me/?user=(username)&w=(weight)";
+  exit;
+}
+
 $username = $_GET['user'];
 $w = isset($_GET['w']) ? (int) $_GET['w'] : 0;
 $h = isset($_GET['h']) ? (int) $_GET['h'] : 0;
 
+if ($w == 0 && $h == 0) 
+  $w = 360;
+
 if ($w != 0 && $h == 0) {
-  $h = ($w / 1080) * 640;
+  $h = ($w / 1450) * 640;
+}
+if ($w == 0 && $h != 0) {
+  $w = ($h / 640) * 1450;
 }
 
 $stars = getTotalStars($username);
 
-$image = imagecreatefrompng('background.png');
+$image = imagecreatefrompng('background1450.png');
 imagesavealpha($image, TRUE);
 
 $text_color = imagecolorallocate($image, 0, 0, 0);
@@ -53,10 +67,12 @@ $text = sprintf('%03d', $stars);
 $c1 = substr($text, -1, 1);
 $c2 = substr($text, -2, 1);
 $c3 = substr($text, -3, 1);
+//$c4 = substr($text, -4, 1);
 
-imagettftext($image, $font_size, 0, 155, 585, $text_color, $font_file, $c3);
-imagettftext($image, $font_size, 0, 450, 585, $text_color, $font_file, $c2);
-imagettftext($image, $font_size, 0, 750, 585, $text_color, $font_file, $c1);
+//imagettftext($image, $font_size, 0, 210, 585, $text_color, $font_file, $c4);
+imagettftext($image, $font_size, 0, 510, 585, $text_color, $font_file, $c3);
+imagettftext($image, $font_size, 0, 810, 585, $text_color, $font_file, $c2);
+imagettftext($image, $font_size, 0, 1110, 585, $text_color, $font_file, $c1);
 
 if ($w != 0 && $h != 0)
   $image2 = imagescale($image, $w, $h);
